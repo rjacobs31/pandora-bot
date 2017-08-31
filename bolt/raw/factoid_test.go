93f3819b9791089998fb025a651c1924db68c087
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2017-08-31 23:29:24 +0200

    Add tests for FactoidService put.

--- bolt/raw/factoid_test.go
+++ bolt/raw/factoid_test.go
@@ -8,6 +8,10 @@ import (
 )
 
 func TestFactoidService(t *testing.T) {
+	var (
+		ok  bool
+		err error
+	)
 	db, close, err := newTestDB()
 	if db == nil || close == nil || err != nil {
 		panic(err)
@@ -25,6 +29,31 @@ func TestFactoidService(t *testing.T) {
 	id, err := s.Create(f)
 	if id == 0 || err != nil {
 		t.Error("Factoid creation failed")
+	}
+
+	f, ok = s.Factoid(id)
+	if !ok || f.ID != id || f.Trigger != "Blah" {
+		t.Error("Factoid retrieval failed")
+	}
+
+	f.Trigger = "Blarg"
+	err = s.Put(id, f)
+	if err != nil {
+		t.Error("Factoid put failed")
+	}
+	f, ok = s.Factoid(id)
+	if !ok || f.Trigger != "Blarg" {
+		t.Error("Factoid put not persisted")
+	}
+
+	f.Trigger = "Honk"
+	err = s.Put(f.ID, f)
+	if err != nil {
+		t.Error("Factoid put by trigger failed")
+	}
+	f, ok = s.FactoidByTrigger("Honk")
+	if !ok || f.Trigger != "Honk" {
+		t.Errorf("Factoid put by trigger not persisted (got %s)", f.Trigger)
 	}
 
 	if err = s.Delete(f.ID); err != nil {
