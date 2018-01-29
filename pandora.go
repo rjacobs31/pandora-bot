88a6aa41ae179dd64267086a6aeca84da2c88559
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-01-29 22:14:03 +0200

    Add basic flag parsing and startup in main.go.

--- pandora.go
+++ pandora.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rjacobs31/pandora-bot/chat"
+)
 
 func main() {
-	fmt.Println("Not yet implemented")
+	config := Config{}
+	flag.StringVar(&config.Token, "token", "", "Discord bot token")
+	flag.Parse()
+
+	log.Println("Starting Discord client")
+	client, err := chat.New(config.Token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.Start()
+
+	log.Println("Opening Discord connection")
+	err = client.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(
+		sc,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		os.Interrupt,
+		os.Kill,
+	)
+	log.Println("Bot running. Press Ctrl+C to exit.")
+	<-sc
 }
