cd78090d5054217d4073e6080b69f35c201e2b75
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-02-14 20:45:15 +0200

    Add factoid manager struct.

--- database/factoids.go
+++ database/factoids.go
@@ -1,11 +1,10 @@
 package database
 
 import (
+	"errors"
+	"math/rand"
+
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mssql"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Remark struct {
@@ -20,4 +19,37 @@ type Retort struct {
 	gorm.Model
 	TriggerCount int
 	Text         string `gorm:"index"`
+}
+
+type FactoidManager struct {
+	db *gorm.DB
+}
+
+func initialiseFactoidManager(db *gorm.DB) FactoidManager {
+	return FactoidManager{db: db}
+}
+
+func (fm *FactoidManager) Add(remark, retort string) (err error) {
+	var rem Remark
+	fm.db.FirstOrInit(&rem, Remark{Text: remark})
+
+	for ret := range rem.Retorts {
+		if ret.Text == retort {
+			return errors.New("Retort already exists")
+		}
+	}
+
+	append(rem.Retorts, Retort{Text: retort})
+	db.Save(&rem)
+}
+
+func (fm *FactoidManager) Select(remark string) (retort Retort, err error) {
+	var rem Remark
+	fm.db.Where("text = ?", remark).First(&rem)
+
+	if len(rem.Retorts) == 0 {
+		return
+	}
+
+	retort = rem.Retorts[rand.Int63n(len(rem.Retorts))]
 }
