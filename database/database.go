47cedf298b2952347dc0f2ef4c7a3b69009dc0d8
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-02-14 19:51:21 +0200

    Add database initialiser.

---
+++ database/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Database struct {
+	*gorm.DB
+}
+
+func Initialise(config Config) (db *Database, err error) {
+	gdb, err := gorm.Open(config.Type, config.ConnectionString)
+	if err != nil {
+		return
+	}
+
+	gdb = db.AutoMigrate(&Remark{}, &Retort{})
+
+	db = &Database{DB: gdb}
+	return
+}
