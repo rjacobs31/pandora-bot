8f3465a89c27ee5094bef96a13f236a0a8bba57d
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-02-15 10:50:57 +0200

    Add skeleton for adding message handlers to chat client.

--- chat/handlers.go
+++ chat/handlers.go
@@ -4,8 +4,16 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+// MessageHandler handles incoming messages using chain of responsibility.
+type MessageHandler interface {
+	Handle(s *discordgo.Session, m *discordgo.MessageCreate)
+	SetNext(n *MessageHandler)
+}
+
 type ChatClient struct {
-	*discordgo.Session
+	session *discordgo.Session
+
+	messageHandlers []MessageHandler
 }
 
 func New(token string) (client *ChatClient, err error) {
@@ -15,16 +23,27 @@ func New(token string) (client *ChatClient, err error) {
 }
 
 func (c *ChatClient) Start() {
-	c.AddHandler(messageCreate)
+	c.session.AddHandler(handleIncomingMessage)
 }
 
 func (c *ChatClient) Close() {
-	c.Session.Close()
+	c.session.Close()
+}
+
+// AddHandler appends a handler to the list of handlers in the
+// chain of responsibility.
+func (c *ChatClient) AddHandler(newHandler MessageHandler) {
+	oldLen := len(c.messageHandlers)
+	if oldLen > 0 {
+		c.messageHandlers[oldLen-1].SetNext(newHandler)
+	}
+
+	append(c.messageHandlers, newHandler)
 }
 
 // messageCreate will be called (by AddHandler) every time a new
 // message is created on any channel that the autenticated bot has access to.
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+func (c *ChatClient) handleIncomingMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
@@ -35,10 +54,4 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Content == "ping" {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
 	}
-}
-
-// MessageHandler handles incoming messages using chain of responsibility.
-type MessageHandler interface {
-	Handle(s *discordgo.Session, m *discordgo.MessageCreate)
-	SetNext(n *MessageHandler)
 }
